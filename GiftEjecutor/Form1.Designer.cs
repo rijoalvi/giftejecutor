debbf1770cc1a1faec1dd4990432e1372e819a76
luiscarlosch <luiscarlosch@eb84f6a0-8826-11de-a332-fd40ed1fd0be>
2009-08-23 06:57:47 +0000

    Ya se estableció la conexión remota de MYSL

--- GiftEjecutor/Form1.Designer.cs
+++ GiftEjecutor/Form1.Designer.cs
@@ -1,6 +1,6 @@
 namespace GiftEjecutor
 {
-    partial class Form1
+    partial class Principal
     {
         /// <summary>
         /// Required designer variable.
@@ -29,6 +29,8 @@ namespace GiftEjecutor
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -40,14 +42,25 @@ namespace GiftEjecutor
             this.label1.TabIndex = 0;
             this.label1.Text = "GiftEjecutor";
             // 
-            // Form1
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 89);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(495, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.ClientSize = new System.Drawing.Size(519, 304);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.Name = "Principal";
             this.Text = "Gift Ejecutor";
+            this.Load += new System.EventHandler(this.FormPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,6 +69,7 @@ namespace GiftEjecutor
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- GiftEjecutor/Form1.cs
+++ GiftEjecutor/Form1.cs
@@ -5,14 +5,47 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
-
+using MySql.Data.MySqlClient;
 namespace GiftEjecutor
 {
-    public partial class Form1 : Form
+    public partial class Principal : Form
     {
-        public Form1()
+        MySqlConnection mySqlConexion = new MySqlConnection("datasource=grupoingegift5.bluechiphosting.com;username=grupoin2_user;password=Qwerty123;database=grupoin2_GiftBD");
+        public Principal()
         {
             InitializeComponent();
         }
+
+        private void FormPrincipal_Load(object sender, EventArgs e)
+        {
+            //string sentenciaMySql = "SELECT * FROM Funcionario;";
+           // MySqlDataReader datos = hacerConsulta(sentenciaMySql);
+            FlujoTrabajo flujo = new FlujoTrabajo();
+            this.dataGridView1.DataSource= flujo.getDataTableTodosLosFlujosDeTrabajo();
+
+        }
+        private MySqlDataReader hacerConsulta(string sentenciaMySql)
+        {
+
+            MySqlDataReader datos = null;
+            mySqlConexion.Open();
+            //bool exito;
+            try
+            {
+                //create a new mysqlconnection
+                MySqlCommand comando = new MySqlCommand(sentenciaMySql, mySqlConexion);
+                datos = comando.ExecuteReader();
+
+            }
+            catch (MySqlException ex)
+            {
+              //  exito = false;
+                MessageBox.Show(ex.Message);
+            }
+
+
+            return datos;
+        }
+
     }
 }
